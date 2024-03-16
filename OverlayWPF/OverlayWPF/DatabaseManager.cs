30d4c7d6a670723b9e18de7384cbdf9b9a456feb
mhrubes <m_hrubes@centrum.cz>
2024-03-16 12:19:34 +0100

    Commit
    Commit

---
+++ OverlayWPF/OverlayWPF/DatabaseManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverlayWPF
+{
+    class DatabaseManager
+    {
+        private SQLiteConnection connection;
+
+        public DatabaseManager()
+        {
+            connection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            connection.Open();
+
+            string createTableQuery = "CREATE TABLE IF NOT EXISTS Data (Id INTEGER PRIMARY KEY, Path TEXT)";
+            ExecuteNonQuery(createTableQuery);
+        }
+
+        public string GetFirstFilePath()
+        {
+            string query = "SELECT Path FROM Data LIMIT 1";
+            using (SQLiteCommand command = new SQLiteCommand(query, connection))
+            {
+                object result = command.ExecuteScalar();
+                if (result != null)
+                {
+                    return result.ToString();
+                }
+                else
+                {
+                    return "Database is empty.";
+                }
+            }
+        }
+
+        public void InsertFilePath(string filePath)
+        {
+            string selectQuery = "SELECT Id FROM Data ORDER BY Id LIMIT 1";
+            using (SQLiteCommand selectCommand = new SQLiteCommand(selectQuery, connection))
+            {
+                object result = selectCommand.ExecuteScalar();
+                if (result != null)
+                {
+                    int firstId = Convert.ToInt32(result);
+                    string updateQuery = "UPDATE Data SET Path = @Path WHERE Id = @Id";
+                    using (SQLiteCommand updateCommand = new SQLiteCommand(updateQuery, connection))
+                    {
+                        updateCommand.Parameters.AddWithValue("@Id", firstId);
+                        updateCommand.Parameters.AddWithValue("@Path", filePath);
+                        updateCommand.ExecuteNonQuery();
+                    }
+                }
+                else
+                {
+                    string insertQuery = "INSERT INTO Data (Path) VALUES (@Path)";
+                    using (SQLiteCommand insertCommand = new SQLiteCommand(insertQuery, connection))
+                    {
+                        insertCommand.Parameters.AddWithValue("@Path", filePath);
+                        insertCommand.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+
+
+        private void ExecuteNonQuery(string query)
+        {
+            using (SQLiteCommand command = new SQLiteCommand(query, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void ExecuteNonQueryWithParameter(string query, string parameterName, string parameterValue)
+        {
+            using (SQLiteCommand command = new SQLiteCommand(query, connection))
+            {
+                command.Parameters.AddWithValue(parameterName, parameterValue);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ OverlayWPF/OverlayWPF/MainWindow.xaml.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Data.SQLite;
+
+namespace OverlayWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isDragging;
+        private Point lastMousePosition;
+
+        public SQLiteConnection connection;
+        public string FilePath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            try
+            {
+                connection.Open();
+
+                DatabaseManager dbManager = new DatabaseManager();
+                string path = dbManager.GetFirstFilePath();
+                FilePath = path;
+
+                if (!string.IsNullOrEmpty(FilePath) && File.Exists(FilePath))
+                {
+                    PathName.Text = FilePath;
+                    ShowFileContent(FilePath);
+                }
+                else
+                {
+                    PathName.Text = "Not Selected .txt file";
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+
+            this.Topmost = true;
+            slider.ValueChanged += Slider_ValueChanged;
+        }
+        private void ShowFileContent(string filePath)
+        {
+            try
+            {
+                string fileContent = File.ReadAllText(filePath, Encoding.UTF8);
+                //Clipboard.SetText(fileContent);
+
+                // Clear existing content
+                textblockText.Inlines.Clear();
+
+                // Define default color
+                Brush defaultColor = Brushes.White;
+
+                // Process each character in the file content
+                for (int i = 0; i < fileContent.Length; i++)
+                {
+                    char currentChar = fileContent[i];
+
+                    // Check for special markers
+                    if (currentChar == '\\')
+                    {
+                        if (i + 1 < fileContent.Length)
+                        {
+                            char nextChar = fileContent[i + 1];
+
+                            // Handle color markers
+                            if (nextChar == 'r')
+                            {
+                                // Set color to red until the next occurrence of '\r'
+                                defaultColor = Brushes.Red;
+
+                                // Find the index of the next occurrence of '\r'
+                                int endIndex = fileContent.IndexOf("\\r", i + 2);
+
+                                // If '\r' is found, process text until the end index
+                                if (endIndex != -1)
+                                {
+                                    for (int j = i + 2; j < endIndex; j++)
+                                    {
+                                        Run run = new Run(fileContent[j].ToString());
+                                        run.Foreground = defaultColor;
+                                        textblockText.Inlines.Add(run);
+                                    }
+                                    i = endIndex + 1; // Skip the '\r' and continue processing
+                                    defaultColor = Brushes.White; // Reset color to default
+                                    continue;
+                                }
+                            }
+                            else if (nextChar == 'g')
+                            {
+                                // Set color to green until the next occurrence of '\g'
+                                defaultColor = Brushes.Green;
+
+                                // Find the index of the next occurrence of '\g'
+                                int endIndex = fileContent.IndexOf("\\g", i + 2);
+
+                                // If '\g' is found, process text until the end index
+                                if (endIndex != -1)
+                                {
+                                    for (int j = i + 2; j < endIndex; j++)
+                                    {
+                                        Run run = new Run(fileContent[j].ToString());
+                                        run.Foreground = defaultColor;
+                                        textblockText.Inlines.Add(run);
+                                    }
+                                    i = endIndex + 1; // Skip the '\g' and continue processing
+                                    defaultColor = Brushes.White; // Reset color to default
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+
+                    // Create a run for the current character with the determined color
+                    Run defaultRun = new Run(currentChar.ToString());
+                    defaultRun.Foreground = defaultColor;
+                    textblockText.Inlines.Add(defaultRun);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+        }
+
+        private void File_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Textové soubory (*.txt)|*.txt|Všechny soubory (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFileName = openFileDialog.FileName;
+                try
+                {
+                    DatabaseManager dbManager = new DatabaseManager();
+                    dbManager.InsertFilePath(selectedFileName);
+
+                    PathName.Text = selectedFileName;
+                    ShowFileContent(selectedFileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Nastala chyba: {ex.Message}");
+                }
+            }
+        }
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
