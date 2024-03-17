3a1cf3e6ebcec2123273aab2a908cb5839a865e2
mhrubes <m_hrubes@centrum.cz>
2024-03-17 14:22:22 +0100

    Update
    Add Icon

--- OverlayWPF/OverlayWPF/obj/Release/net7.0-windows/MainWindow.g.i.cs
+++ OverlayWPF/OverlayWPF/obj/Release/net7.0-windows/MainWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace OverlayWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/OverlayWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/OverlayWPF;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
